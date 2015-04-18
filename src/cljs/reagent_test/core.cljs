39f58a0d24522b3f20903ec168d15e4e9ada26ac
Wojciech Ziniewicz <wojciech.ziniewicz@gmail.com>
2015-04-19 00:10:02 +0200

    Basic routes

--- src/cljs/reagent_test/core.cljs
+++ src/cljs/reagent_test/core.cljs
@@ -15,17 +15,20 @@
   (reagent/atom
    {:bets
     [
-     {:fact "Winner of presidential elections in Poland in April"
+     {:id 1
+      :fact "Winner of presidential elections in Poland in April"
       :title "Who will win presidential elections?"
       :answers ["BK", "AD", "MO"]
       :solves_by 1428675249
       :creator "wojciech.ziniewicz@gmail.com"}
-     {:fact "Winner party in parlamentary elections in Poland at 10.10.2015?"
+     {:id 2
+      :fact "Winner party in parlamentary elections in Poland at 10.10.2015?"
       :solves_by 1428675249
       :title "Which party wins elections in Poland?"
       :answers ["PO", "PiS", "SLD"]
       :creator "Wojtek"}
-     {:fact "Will Poland adopt Euro currency by 01.2016?"
+     {:id 3
+      :fact "Will Poland adopt Euro currency by 01.2016?"
       :title "Will Poland adopt Euro currency by 01.2016?"
       :answers ["yes" "no"]
       :solves_by 1428675249
@@ -41,7 +44,7 @@
 (defn profile-dropdown-item []
   [:ul {:id "profile-dropdown" :class "dropdown-content"}
    [:li
-    [:a {:href "#/finance"} "Finance"]]
+    [:a {:href "#/finances"} "Finances"]]
    [:li
     [:a {:href "#/profile"} "Profile"]]
    ]
@@ -70,7 +73,7 @@
        [:a {:href "#!" :class "brand-logo"} "Bababet"]
        [:ul {:class "right hide-on-med-and-down"}
         [:li
-         [:a {:href "#/home"} "Home"]]
+         [:a {:href "#/"} "Home"]]
         [:li
          [:a {:href "#/bet"} "Bet"]]
         [:li
@@ -88,25 +91,52 @@
 )
 
 ;;
-
-(defn home-page []
+(defn landing-page []
   [:div
    [navbar]
-   [:div [:a {:href "#/about"} "go to about page"]]
+   [:div "Landing page"]
   ]
 )
 
-(defn about-page []
+(defn signin-page []
   [:div
    [navbar]
-   [:div [:a {:href "#/"} "go to the home page"]]
+   [:div "Signin page"]
+  ]
+)
+
+(defn signup-page []
+  [:div
+   [navbar]
+   [:div "Signup page"]
+  ]
+)
+
+(defn propose-page []
+  [:div
+   [navbar]
+   [:div "Propose page"]
+  ]
+)
+
+(defn bet-page [id]
+  [:div
+   [navbar]
+   [:div (str "Bet page: " id)]
   ]
 )
 
 (defn profile-page []
   [:div
    [navbar]
-   [:div [:a {:href "#/"} "go to the home page"]]
+   [:div "Profile"]
+  ]
+)
+
+(defn finances-page []
+  [:div
+   [navbar]
+   [:div "Finances"]
   ]
 )
 
@@ -119,17 +149,39 @@
 
 ;; -------------------------
 ;; Routes
+
 (secretary/set-config! :prefix "#")
 
 (secretary/defroute "/" []
-  (session/put! :current-page #'home-page))
+  (session/put! :current-page #'landing-page))
 
-(secretary/defroute "/about" []
-  (session/put! :current-page #'about-page))
+(secretary/defroute "/signin" []
+  (session/put! :current-page #'signin-page))
+
+(secretary/defroute "/signup" []
+  (session/put! :current-page #'signup-page))
+
+(secretary/defroute "/propose" []
+  (session/put! :current-page #'propose-page))
+
+(secretary/defroute "/bet/:id" {:as params}
+  (session/put! :current-page #'bet-page)
+  (js/console.log (str "Bet: " (:id params)))
+  )
+
+(secretary/defroute "/profile" []
+  (session/put! :current-page #'profile-page))
+
+(secretary/defroute "/finances" []
+  (session/put! :current-page #'finances-page))
 
 ;; -------------------------
 ;; History
 ;; must be called after routes have been defined
+(defn activate-dropdowns []
+  (.dropdown ($ ".profile-dropdown-navbar-item"))
+  (.dropdown ($ ".signin-dropdown-navbar-item"))
+             )
 (defn hook-browser-navigation! []
   (doto (History.)
     (events/listen
@@ -144,10 +196,6 @@
 (defn mount-root []
   (reagent/render [current-page] (.getElementById js/document "app")))
 
-(defn activate-dropdowns []
-  (.dropdown ($ ".profile-dropdown-navbar-item"))
-  (.dropdown ($ ".signin-dropdown-navbar-item"))
-             )
 
 ;;$(".profile-dropdown-navbar-item").dropdown()
 
