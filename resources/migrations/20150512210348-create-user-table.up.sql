f78e78390db94bbb830a281788d52fb76a08165e
Jan <mrholograham@gmail.com>
2015-05-15 01:27:10 +0200

    Move migrations to resources

--- resources/migrations/20150512210348-create-user-table.up.sql
+++ resources/migrations/20150512210348-create-user-table.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE "user" (
   login varchar(80) NOT NULL,
   password varchar(4096) NOT NULL,
   email varchar(1024) NOT NULL,
-  active boolean NOT NULL,
+  "is-active" boolean NOT NULL,
   CONSTRAINT id_pkey PRIMARY KEY (id)
 );
